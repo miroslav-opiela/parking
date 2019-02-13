630d1c37eda0638b13de9d94a3b161df94b804fd
miro <miroslav.opiela@upjs.sk>
2019-02-13 13:24:52 +0100

    upraveny launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,24 +1,6 @@
 public class Launcher {
 
     public static void main(String[] args) {
-        String s = new String("abc");
-        String t = "efg";
-
-        System.out.println(s);
-
-        Car blueCar = new Car();
-        System.out.println(blueCar);
-        blueCar.setCarId("KE123BA");
-        blueCar.setTimeIn(280);
-        blueCar.setHasCard(true);
-
-        Car redCar = new Car("PO111FO", 350, false);
-
-        String carId = blueCar.getCarId();
-
-
-
-
         CarPark parkovisko = new CarPark(110);
 
         // toto mi vypise 0
